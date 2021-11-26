282745d5ce51392aef656d9c90ab79e86f97b97a
Sonja Pylvainen <sonjapylvainen@gmail.com>
2021-11-26 17:13:28 -0600

    feat: add app.js in server file

---
+++ server/app.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 8080;
+
+module.exports = app;
+
+
+// logging middleware
+app.use(morgan("dev"));
+
+// body parsing middleware
+app.use(express.json());
+
+// auth and api routes
+// app.use("/auth", require("./auth"));
+// app.use("/api", require("./api"));
+
+// any remaining requests with an extension (.js, .css, etc.) send 404
+// app.use((req, res, next) => {
+//   if (path.extname(req.path).length) {
+//     const err = new Error("Not found");
+//     err.status = 404;
+//     next(err);
+//   } else {
+//     next();
+//   }
+// });
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
+});
+
+app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+
+app.get('/', (req, res) => res.send('hi kiki teacher'));
